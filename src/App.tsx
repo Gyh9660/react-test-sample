ef596df1d219e137607da12107092b4601c9fc07
gyh <yhgwon@nextree.io>
2025-09-03 17:08:16 +0900

    react-query 일단추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import {createRouter, RouterProvider} from '@tanstack/react-router';
 import {routeTree} from './routeTree.gen';
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
 
 const router = createRouter({routeTree});
+const queryClient = new QueryClient();
 
 export function App() {
-    return <RouterProvider router={router}/>;
+
+    return (
+        <QueryClientProvider client={queryClient}>
+
+            <RouterProvider router={router}/>
+        </QueryClientProvider>
+
+    );
+
 }
